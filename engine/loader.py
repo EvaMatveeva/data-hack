d01300b521c48f26387ebb65b04fbdadd9312e13
daemonix <shmurjovdd@gmail.com>
2022-08-06 13:45:00 +0300

    csv reader update

--- engine/loader.py
+++ engine/loader.py
@@ -1,8 +1,32 @@
 from abc import ABC, abstractmethod
+import pandas as pd
 
-LOAD_TYPE_TO_LOADER_CLS = {}
+class AbstractLoader(ABC):
+    def __init__(self, config) -> None:
+        super().__init__()
+        self.fields = config["fields"]
+        self.target_table = config["target_table"]
 
-class AbstractReader(ABC):
+
     @abstractmethod
-    def load(config, data):
+    def load(self, df: pd.DataFrame):
         pass
+
+
+class TestLoader(AbstractLoader):
+    def __init__(self, config) -> None:
+        super().__init__(config)
+        self.first_batch = True
+
+    def load(self, df: pd.DataFrame):
+        if self.first_batch:
+            df.to_csv(self.target_table, index=False)
+            self.first_batch = False
+        else:
+            df.to_csv(self.target_table, mode='a', header=False, index=False)
+        
+
+
+LOAD_TYPE_TO_LOADER_CLS = {
+    "test" : TestLoader
+}
--- engine/runner.py
+++ engine/runner.py
@@ -5,9 +5,15 @@ from loader import LOAD_TYPE_TO_LOADER_CLS
 class Runner:
     def __init__(self, config) -> None:
         source_type = config["source_type"]
+        if source_type not in SOURCE_TYPE_TO_READER_CLS:
+            raise ValueError(f"Unsupported source_type {source_type}")
+
+        load_type = config["load_type"]
+        if source_type not in SOURCE_TYPE_TO_READER_CLS:
+            raise ValueError(f"Unsupported source_type {load_type}")
 
         self.reader = SOURCE_TYPE_TO_READER_CLS[source_type](config)
-        self.loader = LOAD_TYPE_TO_LOADER_CLS[source_type](config)
+        self.loader = LOAD_TYPE_TO_LOADER_CLS[load_type](config)
 
     def run(self):
         for df in self.reader.read():
