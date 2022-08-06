133c3eb9e5f8d91b40214984ff976ecd79070768
daemonix <shmurjovdd@gmail.com>
2022-08-06 12:43:43 +0300

    csv reader add

--- engine/reader.py
+++ engine/reader.py
@@ -1,8 +1,32 @@
 from abc import ABC, abstractmethod
 
-SOURCE_TYPE_TO_READER_CLS = {}
+import pandas as pd
+
 
 class AbstractReader(ABC):
+    def __init__(self, config) -> None:
+        super().__init__()
+        self.fields = config["fields"]
+        self.distr_columns = config["distr_column"]
+        self.fields = config["fields"]
+        self.batch_size = config["batch_size"]
+
     @abstractmethod
-    def read(config):
+    def read(self):
         pass
+    
+
+class CSVReader(AbstractReader):
+    def __init__(self, config) -> None:
+        super().__init__(config)
+        self.file_path = config["file_path"]
+
+    def read(self):
+        with pd.read_csv(self.file_path, chunksize=self.batch_size) as reader:
+            for batch in reader:
+                yield batch
+
+
+SOURCE_TYPE_TO_READER_CLS = {
+    "csv" : CSVReader,
+}
